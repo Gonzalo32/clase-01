49d9adf3ae6dad03050167df012a13e90006be1e
Gonzalo Guerrero <gonza93nqn@gmail.com>
2024-03-11 22:18:31 -0300

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'clase-01';
+  title = 'clase-01 de Gonzalo Guerrerogit g';
 }
